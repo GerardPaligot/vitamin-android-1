1e9081da7593145148a58105cf493edcf2084159
Gerard Paligot <gerard.paligot@gmail.com>
2022-05-18 17:17:51 +0200

    refactor(chips): Apply new naming convention for ids.

--- chips/src/main/java/com/decathlon/vitamin/chips/VitaminChips.kt
+++ chips/src/main/java/com/decathlon/vitamin/chips/VitaminChips.kt
@@ -7,59 +7,59 @@ import com.google.android.material.chip.Chip
 class VitaminFilterMediumChip @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
-    defStyleAttr: Int = R.attr.chipFilterMedium
+    defStyleAttr: Int = R.attr.vtmnChipFilterMedium
 ) : Chip(context, attrs, defStyleAttr)
 
 class VitaminFilterSmallChip @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
-    defStyleAttr: Int = R.attr.chipFilterSmall
+    defStyleAttr: Int = R.attr.vtmnChipFilterSmall
 ) : Chip(context, attrs, defStyleAttr)
 
 class VitaminInputMediumChip @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
-    defStyleAttr: Int = R.attr.chipInputMedium
+    defStyleAttr: Int = R.attr.vtmnChipInputMedium
 ) : Chip(context, attrs, defStyleAttr)
 
 class VitaminInputImageMediumChip @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
-    defStyleAttr: Int = R.attr.chipInputImageMedium
+    defStyleAttr: Int = R.attr.vtmnChipInputImageMedium
 ) : Chip(context, attrs, defStyleAttr)
 
 class VitaminInputSmallChip @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
-    defStyleAttr: Int = R.attr.chipInputSmall
+    defStyleAttr: Int = R.attr.vtmnChipInputSmall
 ) : Chip(context, attrs, defStyleAttr)
 
 class VitaminInputImageSmallChip @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
-    defStyleAttr: Int = R.attr.chipInputImageSmall
+    defStyleAttr: Int = R.attr.vtmnChipInputImageSmall
 ) : Chip(context, attrs, defStyleAttr)
 
 class VitaminSingleChoiceMediumChip @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
-    defStyleAttr: Int = R.attr.chipSingleChoiceMedium
+    defStyleAttr: Int = R.attr.vtmnChipSingleChoiceMedium
 ) : Chip(context, attrs, defStyleAttr)
 
 class VitaminSingleChoiceSmallChip @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
-    defStyleAttr: Int = R.attr.chipSingleChoiceSmall
+    defStyleAttr: Int = R.attr.vtmnChipSingleChoiceSmall
 ) : Chip(context, attrs, defStyleAttr)
 
 class VitaminActionMediumChip @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
-    defStyleAttr: Int = R.attr.chipActionMedium
+    defStyleAttr: Int = R.attr.vtmnChipActionMedium
 ) : Chip(context, attrs, defStyleAttr)
 
 class VitaminActionSmallChip @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
-    defStyleAttr: Int = R.attr.chipActionSmall
+    defStyleAttr: Int = R.attr.vtmnChipActionSmall
 ) : Chip(context, attrs, defStyleAttr)
